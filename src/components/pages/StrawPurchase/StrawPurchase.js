3c81dc901bad480dbcd913abc27a62e54d8187f2
davidhudman <12885667+davidhudman@users.noreply.github.com>
2022-12-07 16:53:21 -0500

    Agent Purchase
    - added a bunch of stuff to "agent" purchases (new term) like refund address, form submission, etc

--- src/components/pages/StrawPurchase/StrawPurchase.js
+++ src/components/pages/StrawPurchase/StrawPurchase.js
@@ -10,117 +10,164 @@ import "./strawpurchase.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const StrawPurchase = () => {
-  const [tipPercentageChecked, setTipPercentageChecked] = useState("option15");
+  const [whichTipPercentageChecked, setWhichTipPercentageChecked] =
+    useState("option0");
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [isShowTip, setIsShowTip] = useState(true);
+  const [tipCustomAmount, setTipCustomAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [formEnabled, setFormEnabled] = useState(true);
+  const [showCustomTip, setShowCustomTip] = useState(false);
+  const [email, setEmail] = useState("");
+  const [qrCodeLink, setQrCodeLink] = useState("");
+  const [refundAddress, setRefundAddress] = useState("");
 
-  const getStrawPurchaseForm = () => {
-    let formEnabled = true;
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setFormEnabled(false);
+
+    // build the payment request
+    const paymentRequest = {
+      tipCustomAmount,
+      tipPercentage,
+      email,
+      qrCodeLink,
+      refundAddress,
+    };
+
+    // send the payment request to the server
+    fetch("/api/agentpurchase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(paymentRequest),
+    })
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          console.log("data", data);
+          setFormEnabled(false);
+        } // end of .then()
+      ); // end of fetch()
+  };
+
+  const getTipForm = () => {
+    return (
+      <>
+        {/* bill amount */}
+        {/* <div className="form-group">
+          <label htmlFor="bill-amount">Bill Amount Before Tip (optional)</label>
+          <br />
+          <small id="bill-amount-help" className="form-text text-muted">
+            We will charge you based on the QR code you send, not the amount
+            entered here. This is just so you can specify a tip.
+          </small>
+          <input
+            type="number"
+            className="form-control"
+            id="bill-amount"
+            placeholder="Enter Bill Amount"
+          />
+        </div> */}
+        {/* tip percentage buttons */}
+        <div className="form-group">
+          <label htmlFor="tip-percentage">Tip Percentage</label>
+          <br />
+          <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <label className="btn btn-secondary active">
+              <input
+                type="radio"
+                name="options"
+                id="option0"
+                autoComplete="off"
+                defaultChecked
+                value="0"
+                onClick={tipPercentageClickHandler}
+              />{" "}
+              0%
+            </label>
+            <label className="btn btn-secondary">
+              <input
+                type="radio"
+                name="options"
+                id="option15"
+                autoComplete="off"
+                value="15"
+                onClick={tipPercentageClickHandler}
+              />{" "}
+              15%
+            </label>
+            <label className="btn btn-secondary">
+              <input
+                type="radio"
+                name="options"
+                id="option18"
+                autoComplete="off"
+                value="18"
+                onClick={tipPercentageClickHandler}
+              />{" "}
+              18%
+            </label>
+            <label className="btn btn-secondary">
+              <input
+                type="radio"
+                name="options"
+                id="option20"
+                autoComplete="off"
+                value="20"
+                onClick={tipPercentageClickHandler}
+              />{" "}
+              20%
+            </label>
+            <label className="btn btn-secondary">
+              <input
+                type="radio"
+                name="options"
+                id="optionother"
+                autoComplete="off"
+                value=""
+                onClick={tipPercentageClickHandler}
+              />{" "}
+              other
+            </label>
+          </div>
+        </div>
+
+        {/* tip amount */}
+        {showCustomTip ? (
+          <div className="form-group">
+            <label htmlFor="tip-amount">Custom Tip Amount (in dollars)</label>
+            <br />
+
+            <input
+              type="number"
+              className="form-control"
+              id="tip-amount"
+              placeholder="Enter Tip Amount"
+              onChange={(e) => setTipCustomAmount(e.target.value)}
+            />
+          </div>
+        ) : null}
+        {/* <small id="custom-tip-help" className="form-text text-muted">
+            Enter any amount you want to tip
+          </small> */}
+        {/* total amount */}
+        {/* <div className="form-group">
+          <label htmlFor="total-amount">Total Amount (in dollars)</label>
+          <h3 id="total-amount">${totalAmount}</h3>
+        </div> */}
+      </>
+    );
+  };
+
+  const getAgentPurchaseForm = () => {
     if (formEnabled) {
       return (
         <>
           {/* form with text box for QR code link */}
           <div className="strawpurchase-form">
             <form>
-              <div className="form-group">
-                <label htmlFor="qr-code-link">QR Code Link</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="qr-code-link"
-                  placeholder="Enter QR Code Link"
-                />
-              </div>
-              {/* bill amount */}
-              <div className="form-group">
-                <label htmlFor="bill-amount">Bill Amount (optional)</label>
-                <br />
-                {/* italic text */}
-                <small id="bill-amount-help" className="form-text text-muted">
-                  We will charge you based on the QR code you send, not the
-                  amount entered here. This is just so you can specify a tip.
-                </small>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="bill-amount"
-                  placeholder="Enter Bill Amount"
-                />
-              </div>
-              {/* tip percentage buttons */}
-              <div className="form-group">
-                <label htmlFor="tip-percentage">Tip Percentage</label>
-                <br />
-                <div
-                  className="btn-group btn-group-toggle"
-                  data-toggle="buttons"
-                >
-                  <label className="btn btn-secondary active">
-                    <input
-                      type="radio"
-                      name="options"
-                      id="option15"
-                      autoComplete="off"
-                      defaultChecked
-                      value="15"
-                      onClick={tipPercentageClickHandler}
-                    />{" "}
-                    15%
-                  </label>
-                  <label className="btn btn-secondary">
-                    <input
-                      type="radio"
-                      name="options"
-                      id="option18"
-                      autoComplete="off"
-                      value="18"
-                      onClick={tipPercentageClickHandler}
-                    />{" "}
-                    18%
-                  </label>
-                  <label className="btn btn-secondary">
-                    <input
-                      type="radio"
-                      name="options"
-                      id="option20"
-                      autoComplete="off"
-                      value="20"
-                      onClick={tipPercentageClickHandler}
-                    />{" "}
-                    20%
-                  </label>
-                  <label className="btn btn-secondary">
-                    <input
-                      type="radio"
-                      name="options"
-                      id="option25"
-                      autoComplete="off"
-                      value="25"
-                      onClick={tipPercentageClickHandler}
-                    />{" "}
-                    25%
-                  </label>
-                </div>
-              </div>
-
-              {/* tip amount */}
-              <div className="form-group">
-                <label htmlFor="tip-amount">Tip Amount (in dollars)</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="tip-amount"
-                  placeholder="Enter Tip Amount"
-                />
-              </div>
-              {/* total amount */}
-              <div className="form-group">
-                <label htmlFor="total-amount">Total Amount (in dollars)</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="total-amount"
-                  placeholder="Enter Total Amount"
-                />
-              </div>
               {/* email */}
               <div className="form-group">
                 <label htmlFor="email">Email</label>
@@ -129,13 +176,48 @@ const StrawPurchase = () => {
                   className="form-control"
                   id="email"
                   placeholder="Enter Email"
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
+              <div className="form-group">
+                <label htmlFor="qr-code-link">QR Code Link</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="qr-code-link"
+                  placeholder="Enter QR Code Link"
+                  onChange={(e) => setQrCodeLink(e.target.value)}
+                />
+              </div>
+              {/* refund address */}
+              <div className="form-group">
+                <label htmlFor="refund-address">Refund Address</label>
+                <br />
+                {/* small italic text */}
+                <small
+                  id="refund-address-help"
+                  className="form-text text-muted"
+                >
+                  If there is a problem with your payment, we will send your
+                  refund to this address.
+                </small>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="refund-address"
+                  placeholder="Enter Refund Address"
+                  onChange={(e) => setRefundAddress(e.target.value)}
+                />
+              </div>
+
+              {getTipForm()}
+
               {/* submit button */}
               <button
                 type="submit"
                 className="btn btn-lg btn-block btn-primary"
                 style={{ fontSize: "18px" }}
+                onClick={(e) => onFormSubmit(e)}
               >
                 Submit
               </button>
@@ -150,18 +232,20 @@ const StrawPurchase = () => {
 
   // tip percentage button click handler
   const tipPercentageClickHandler = (e) => {
-    // disable default checked for previously checked button
-    console.log(`changing ${tipPercentageChecked} to ${e.target.id}`);
+    setShowCustomTip(e.target.value === "" ? true : false);
 
-    document.getElementById(tipPercentageChecked).checked = false;
-    document.getElementById(tipPercentageChecked).defaultChecked = false;
+    // disable default checked for previously checked button
+    console.log(`changing ${whichTipPercentageChecked} to ${e.target.id}`);
+
+    document.getElementById(whichTipPercentageChecked).checked = false;
+    document.getElementById(whichTipPercentageChecked).defaultChecked = false;
     // remove active class from previously checked button's label
     document
-      .getElementById(tipPercentageChecked)
+      .getElementById(whichTipPercentageChecked)
       .parentNode.classList.remove("active");
 
     // set new checked button
-    setTipPercentageChecked(e.target.id);
+    setWhichTipPercentageChecked(e.target.id);
     document.getElementById(e.target.id).checked = true;
     document.getElementById(e.target.id).defaultChecked = true;
 
@@ -171,28 +255,34 @@ const StrawPurchase = () => {
     // set target to defaultChecked
     e.target.defaultChecked = true;
 
-    // get the tip percentage
-    const tipPercentage = e.target.value;
+    // set tip percentage
+    setTipPercentage(e.target.value);
 
-    // get the bill amount
-    const billAmount = document.getElementById("bill-amount").value;
+    // // get the tip percentage
+    // const tipPercentage = e.target.value;
 
-    // calculate the tip amount
-    const tipAmount = (billAmount * tipPercentage) / 100;
+    // // get the bill amount
+    // const billAmount = document.getElementById("bill-amount").value;
 
-    // calculate the total amount
-    const totalAmount = parseFloat(billAmount) + parseFloat(tipAmount);
+    // // calculate the tip amount
+    // const tip_amount = (billAmount * tipPercentage) / 100;
 
-    // display the tip amount
-    document.getElementById("tip-amount").value = tipAmount;
+    // // calculate the total amount
+    // const total_amount = parseFloat(billAmount) + parseFloat(tip_amount);
 
-    // display the total amount
-    document.getElementById("total-amount").value = totalAmount;
+    // // display the tip amount
+    // document.getElementById("tip-amount").value = tip_amount;
+
+    // // display the total amount
+    // document.getElementById("total-amount").value = total_amount;
+
+    // setTotalAmount(total_amount);
   };
+
   return (
     <div className="outer-home-container">
       <div className="home">
-        <h1>Straw Purchases</h1>
+        <h1>Agent Purchases</h1>
         <h4>Send a QR-enabled receipt & crypto, and we pay the restaurant.</h4>
 
         <br />
@@ -213,13 +303,13 @@ const StrawPurchase = () => {
           <br />
         </div>
 
-        {getStrawPurchaseForm()}
+        {getAgentPurchaseForm()}
 
         <br />
         <br />
 
         <p>
-          Straw purchases are a way to pay for your meal at a restaurant that
+          Agent purchases are a way to pay for your meal at a restaurant that
           has QR code payments on their bill. You can pay for your meal by
           asking for your bill and scanning it with your phone. Then you can
           send the link to us, and we will request the equivalent amount of
