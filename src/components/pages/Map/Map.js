42afeefa8847b0a3eae3dc49032647e531d368a4
davidhudman <12885667+davidhudman@users.noreply.github.com>
2022-12-14 18:36:41 -0500

    add get merchants as list

--- src/components/pages/Map/Map.js
+++ src/components/pages/Map/Map.js
@@ -21,8 +21,93 @@ const loadGoogleMapScript = (callback) => {
 
 const App = () => {
   const [loadMap, setLoadMap] = useState(false);
+  const [merchants, setMerchants] = useState([]);
+  const [showMerchantList, setShowMerchantList] = useState(false);
+
+  const showHideMerchantList = () => {
+    setShowMerchantList(!showMerchantList);
+  };
+
+  const getMerchants = async () => {
+    const response = await fetch(
+      "https://j8tmnngcj5.execute-api.us-east-1.amazonaws.com/default/bchMerchant01"
+    );
+    const data = await response.json();
+
+    // prepend hard coded merchants
+    data.unshift(
+      {
+        merchantName: "Commercial Kitchen Stop",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "597",
+        category: "Restaurant",
+        partnerLevel: 4,
+        coordinates: { lat: 26.64435, lng: -80.08751 },
+        location: "2180 S Congress Ave Unit A, Palm Springs, FL 33406",
+        phone: "(888) 219-8045",
+        website: "commercialkitchenstop.com",
+      },
+      {
+        merchantName: "Oli's Fashion Cuisine",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "321",
+        category: "Restaurant",
+        partnerLevel: 1,
+        coordinates: { lat: 26.65018, lng: -80.21585 },
+        location: "10610 Forest Hill Blvd #20, Wellington, FL 33414",
+        phone: "(561) 792-2220",
+        website: "olisfashioncuisine.com",
+      },
+      {
+        merchantName: "Dibartolo's Wholesale Food Warehouse",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "125",
+        category: "Restaurant",
+        partnerLevel: 3,
+        coordinates: { lat: 26.69091, lng: -80.17668 },
+        location: "8140 Belvedere Rd, West Palm Beach, FL 33411",
+        phone: "(561) 814-2988",
+        website: "",
+      },
+      {
+        merchantName: "Marbar Grille",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "248",
+        category: "Restaurant",
+        partnerLevel: 2,
+        coordinates: { lat: 26.69448, lng: -80.24433 },
+        location: "2001 Crestwood Blvd N, Royal Palm Beach, FL 33411",
+        phone: "(561) 784-5225",
+        website: "",
+      },
+      {
+        merchantName: "Hobo's Gourmet Kitchen",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "511",
+        category: "Restaurant",
+        partnerLevel: 4,
+        coordinates: { lat: 26.80773, lng: -80.05936 },
+        location: "421 Northlake Blvd, North Palm Beach, FL 33408",
+        phone: "(561) 841-8305",
+        website: "hobosgourmetkitchen.net",
+      },
+      {
+        merchantName: "Cucina Palm Beach Gardens",
+        lastTransaction: "2022-05-20",
+        transactionslast30days: "492",
+        category: "Restaurant",
+        partnerLevel: 1,
+        coordinates: { lat: 26.8359586, lng: -80.131431 },
+        location: "7100 Fairway Dr, FL-786 #61A, Palm Beach Gardens, FL 33418",
+        phone: "(561) 557-9510",
+        website: "cucinapbg.com",
+      }
+    );
+    setMerchants(data);
+  };
 
   useEffect(() => {
+    getMerchants();
     loadGoogleMapScript(() => {
       setLoadMap(true);
     });
@@ -30,7 +115,25 @@ const App = () => {
 
   return (
     <div className="App">
-      <h2>Bitcoin Cash Merchant Map</h2>
+      <h2>Crypto Merchant Map</h2>
+      <button
+        className="btn btn-lg btn-block btn-default"
+        onClick={showHideMerchantList}
+      >
+        Map View / List View
+      </button>
+      {showMerchantList ? (
+        <div>
+          {merchants.map((merchant) => (
+            <div className="merchant" key={merchant.merchantId}>
+              <h3>{merchant.merchantName}</h3>
+              <p>Location: {merchant.location ? merchant.location : "N/A"}</p>
+              {/* horizontal line */}
+              <hr />
+            </div>
+          ))}
+        </div>
+      ) : null}
       {!loadMap ? <div>Loading...</div> : <GMap />}
       <br />
       <br />
